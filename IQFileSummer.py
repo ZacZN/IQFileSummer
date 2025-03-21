dd8a0259d6a896a4dad9946db5dc8d72bcba1941
Zac N <z.nunns@surrey.ac.uk>
2025-03-21 16:34:42 +0100

    rearranged code to ensure all variables are defined correctly

--- IQFileSummer.py
+++ IQFileSummer.py
@@ -15,7 +15,7 @@ def parse_dataset(dataset):
     return data_arr
 
 
-def data_summer(dataset, path, output_name, output_location):
+def data_summer(dataset, path, output_location):
     zz = np.array([])
     for filename in dataset:
         fullpath = path + filename[0]
@@ -37,26 +37,26 @@ def data_summer(dataset, path, output_name, output_location):
 
     xx, yy, _ = iq.get_power_spectrogram(lframes=lframes, nframes=nframes)
 
+    output_name = f"{experiment_name}_{t_start}-{t_end}_{lframes}lframes-{nframes}nframes"
+
     print(f"Saving data to file {output_name}.npz in location {output_location}")
     np.savez(output_name + ".npz", xx, yy, np.abs(np.fft.fftshift(zz, axes=1)))
 
+t_start = config["settings"]["t_start"]
+t_end = config["settings"]["t_end"]
+lframes = config["settings"]["lframes"]
+lframes = int(lframes)
+experiment_name = config["settings"]["experiment_name"]
+output_location = config["settings"]["output_location"]
+
+file_list = config["settings"]["file_list"]
+file_path = config["settings"]["file_path"]
+
 def main():
-
-    t_start = config["settings"]["t_start"]
-    t_end = config["settings"]["t_end"]
-    lframes = config["settings"]["lframes"]
-    nframes = int(iq.nsamples_total / lframes)
-    experiment_name = config["settings"]["experiment_name"]
-    output_location = config["settings"]["output_location"]
-
-    output_name = f"{experiment_name}_{t_start}-{t_end}_{lframes}lframes-{nframes}nframes"
-
-    file_list = config["settings"]["file_list"]
-    file_path = config["settings"]["file_path"]
 
     dataset = parse_dataset(file_list)
 
-    data_summer(dataset=dataset, path=file_path, output_name=output_name, output_location=output_location)
+    data_summer(dataset=dataset, path=file_path, output_location=output_location)
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Set the desired settings in the config file, then run the script:
 
 The different settings in the config file are as follows:
 
-    * `file_list` - the list of data files to sum together. Format is a text file with the name of each file on a new line. Type: str
-    * `file_path` - path to the data files. Type: str
-    * `output_location` - location where you want to save the .npz output file. Type: str
-    * `lframes` - frame length, _i.e._ the number of frequency bins. This then determines the value of nframes (number of time bins). Type: int
-    * `t_start` - time of the first data file, used to generate the name of the .npz file. Type: str
-    * `t_end` - time of the last data file, also used for name generation. Type: str
-    * `experiment_name` - name of the experiment, also for name generation. Type: str
+    * `file_list` - the list of data files to sum together. Format is a text file with the name of each file on a new line.
+    * `file_path` - path to the data files.
+    * `output_location` - location where you want to save the .npz output file.
+    * `lframes` - frame length, _i.e._ the number of frequency bins. This then determines the value of nframes (number of time bins).
+    * `t_start` - time of the first data file, used to generate the name of the .npz file.
+    * `t_end` - time of the last data file, also used for name generation.
+    * `experiment_name` - name of the experiment, also for name generation.
